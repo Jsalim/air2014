df21c8b48e646da36c9e98d4d365bc66d5fd1884
Paris Mavromoustakos <parismav87@gmail.com>
2014-02-25 16:48:30 +0100

    optimizeAcquisitionFunction

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #air 2014 group 5. (REMBO)
 
+#step 1: generate a random matrix
+
+
+#step2:choose the bounding region.
+
+
+
+for (t in range(0,100)):
+	#step4
+
+	#step5
+
---
+++ optimizeAcquisitionFunction.py
@@ -0,0 +1,24 @@
+#step4 
+
+import numpy as np
+
+
+#X=box t=current timestep D=data
+def optimizeAcquisitionFunction(t,X,D):
+
+	#initialize the max
+	maxX = X.item(0);
+
+	for(x in X):
+		#u = aqcuisition function
+		y = u(x,D)
+
+		#check for max
+		if(y>maxX):
+			maxX=y
+
+	#maxX is the x that optimizes the u function
+	return maxX
+
+
+
