0da40a426612db4078ec664656e8e2099103e332
jeromecremers <jerome.cremers@student.uva.nl>
2014-02-25 18:26:36 +0100

    added some stuff

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #air 2014 group 5. (REMBO)
 
 #step 1: generate a random matrix
+import create_random_matrix as crm
+D = 64 #number of features?
+d = 3 #parameter
 
+print crm.random_matrix(D,d)
 
 #step2:choose the bounding region.
 
 
 
-for (t in range(0,100)):
+#for (t in range(0,100)):
 	#step4
 
 	#step5
