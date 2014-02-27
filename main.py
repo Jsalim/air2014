4f4f926d3f9ad4e6ea0b077778f7369048ec3798
Paris Mavromoustakos <parismav87@gmail.com>
2014-02-27 15:58:44 +0100

    wrote some example pseudo-code

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ d = 3 #parameter
 
 print crm.random_matrix(D,d)
 
+
+
+#paris - this code is general (doesnt compile)
+
+for (each d we experiment with):
+	A = randomMatrix(D,d);
+	Y = randomVector(d) #values from -sqrt(d) to sqrt(d)
+ 	x1,x2 =randomPointsOfY(); #get 2 random points inside Y.
+ 	ranker1Features =x1*A;
+ 	ranker2Features = x2*A
+ 	while(nextQuery):
+ 		winningRanker = interleave(ranker1,ranker2,someClickModel(),randomQuery()); 
+ 		updateWeights(winningRanker);
+
+
+
+#end paris code
+
+
 #step2:choose the bounding region.
 
 
