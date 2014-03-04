619004b850ea651266e1b58df25012bbcc5e91e8
Arif Qodari <arif.qodari@gmail.com>
2014-03-04 18:34:14 +0100

    The concatenation of covariance matrix still doesn't work

--- main_rembo.py
+++ main_rembo.py
@@ -12,7 +12,7 @@ d = 3 # reducted dimension
 
 n_training = 10
 n_test = 1
-max_iter = 1000 # maximum number of iterations
+max_iter = 1 # maximum number of iterations
 sigma_0 = 0.1
 
 regionBound = math.sqrt(d)
@@ -20,7 +20,7 @@ regionBoundStepSize = 0.1
 
 
 # Sample training input and ouput
-ytrain = np.matrix(np.random.uniform(-5, 5, (n_training, D)))
+ytrain = np.random.uniform(-5, 5, (n_training, D))
 fytrain = acq.sample_training_output(ytrain)
 
 
@@ -34,7 +34,8 @@ Y = cbr.chooseBoundedRegion(d, -regionBound, regionBound, regionBoundStepSize)
 
 #define initial mu and sigma
 mu =0
-sigma = 1 
+sigma = [[1]]
+
 # Step 3 - 6
 for t in range(0, max_iter):
   # Select points from bounded box to be tested
@@ -42,10 +43,10 @@ for t in range(0, max_iter):
   #ytest = acq.select_test_set(n_test, Y)
 
   # Get mu and sigma
-  mu, sigma = acq.gp_posterior(ytrain, sigma, ytest, sigma_0, n_test, A, t)
+  mu, sigma, ybest = acq.gp_posterior(ytrain, sigma, ytest, fytrain, A, t, d)
 
   # Find ybest
-  ybest = acq.gp_optimize(ytest, t, D, mu, sigma, n_test)
+  # ybest = acq.gp_optimize(ytest, t, D, mu, sigma, n_test)
 
   # Augment the data
   ytrain, fytrain = acq.augment_data(ytrain, fytrain, ybest, A)
