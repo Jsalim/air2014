1bef5b10af8267d86bc2c8a4a544c1c3d1d1c23c
Arif Qodari <arif.qodari@gmail.com>
2014-03-04 18:34:35 +0100

    The concatenation of covariance matrix still doesn't work

--- create_random_matrix.py
+++ create_random_matrix.py
@@ -4,4 +4,4 @@ import numpy as np
 # with mean = 0, and standar deviation = 1
 # REMBO paper page 6
 def random_matrix(D, d):
-  return np.matrix(np.random.normal(0, 1, (D, d)))
+  return np.random.normal(0, 1, (D, d))
