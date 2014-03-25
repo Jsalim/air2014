d91f31931003836e69fa71359e999b6c214edde5
Paris Mavromoustakos <parismav87@gmail.com>
2014-03-25 14:00:03 +0100

    output-500 & png & matlab file

---
+++ lerot/output-500/experiment1/textToCurve.m
@@ -0,0 +1,14 @@
+%A = dmlread('sampleExperiment.txt');
+%file = fopen('sampleExperiment.txt');
+%tline = fgetl(file);
+%A= tline(3:end);
+%C = strsplit(A,',');
+%A=fscanf(file,'%f %f',[7 inf]);
+%A=A';
+%plot(A);
+%s = '[1.5,2.3,4.0,9.1]'
+
+%eval(['x = {' s '}']);
+text = fileread('k1d3.txt');
+eval(['x = [' text ']']);
+plot(x);
